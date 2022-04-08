41413959817b5bc581fc40e111bd893f42abcdf5
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-04-08 10:00:36 +0200

    beautifycode

--- assets/app.js
+++ assets/app.js
@@ -5,15 +5,15 @@
 // Hay que tener el servidor de Tomcat en el intelliJ encendido
 
 fetch("http://localhost:8070/movies/") // Manda el request
-.then(respuesta =>{ // La API retorna una respuesta, parse response as JSON
-  // console.log(respuesta.json());
- return respuesta.json() 
-})
-.then(data => {
-  console.log(data)
-  // document.querySelector('img').src = data.coverImage
-  // document.querySelector('h1').innerHTML = data.director
-})
-.catch(err => {
+  .then(respuesta => { // La API retorna una respuesta, parse response as JSON
+    // console.log(respuesta.json());
+    return respuesta.json()
+  })
+  .then(data => {
+    console.log(data)
+    // document.querySelector('img').src = data.coverImage
+    // document.querySelector('h1').innerHTML = data.director
+  })
+  .catch(err => {
     console.log(`error ${err}`)
-})
+  })
