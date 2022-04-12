eed3234e5b984a678f0befb242ef0d7ff66c5892
Miguel Iñiguez <maip202@gmail.comgit config --global user.name maip202>
2022-04-12 12:30:15 +0200

    fetch/movies

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,43 @@
-// Si fetch es para hacer lo mismo que postman, mandar requests a la API
-// Por defecto si no le pasamos el método, hace request con el método GET
 
-// Hay que pasarle la ruta : http://localhost:8070/movies/
-// Hay que tener el servidor de Tomcat en el intelliJ encendido
+getMovies();
 
-fetch("http://localhost:8070/movies/") // Manda el request
-  .then(respuesta => { // La API retorna una respuesta, parse response as JSON
-    // console.log(respuesta.json());
-    return respuesta.json()
-  })
-  .then(data => {
-    console.log(data)
-    // document.querySelector('img').src = data.coverImage
-    // document.querySelector('h1').innerHTML = data.director
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  })
+function getMovies() {
+    fetch("http://localhost:8080/movies")
+    .then(response => response.json())
+    .then(data =>  {
+        console.log(data);
+        printMovies(data);
+
+    });
+}
+
+function printMovies(data) {
+    // Capturar el elemento HTML div con el id miLista
+    let moviesListUlElement = document.getElementById("main");
+
+    // Crear un elemnto HTML ul en JavaScript
+    let ulElement = document.createElement("div");
+
+    // Añadimos el elemento creado ul al elemento div (miLista)
+    moviesListUlElement.appendChild(ulElement);
+
+    data.forEach(movie => {
+        // crear el HTML necesario para imprimir una peli
+        let listaHtml = '<h1 class="ejemplo" style="color: red;">'+movie.title+'</h1>';
+        let directoHtml = '<h2 class="ejemplo" style="color: blue;">'+movie.director+'</h2>';
+        let scoreHtml = '<h2 class="ejemplo" style="color: black;">'+movie.score+'</h2>';
+        let synopsisHtml = '<h2 class="ejemplo" style="color: black;">'+movie.synopsis+'</h2>';
+        let coverImage =   '<img src='+movie.coverImage+' class="img-fluid" alt="">'
+
+        //li.appendChild(document.createTextNode(movie.title));
+        //moviesListUlElement.appendChild(listaHtml);
+        
+        // Añadimos el elemento creado (li) al elemento ul
+        ulElement.innerHTML += coverImage;
+        ulElement.innerHTML += listaHtml;
+        ulElement.innerHTML += directoHtml;
+        ulElement.innerHTML += scoreHtml;
+        ulElement.innerHTML += synopsisHtml;
+    });
+}
+
