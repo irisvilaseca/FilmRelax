01771ccbe18b9fd58df84c2fd6b956949e0100f3
Iris <iris.vilaseca1998@gmail.com>
2022-04-12 14:12:22 +0200

    better structure

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@
 getMovies();
 
 function getMovies() {
-    fetch("http://localhost:8080/movies")
+    fetch("http://localhost:8090/movies")
     .then(response => response.json())
     .then(data =>  {
         console.log(data);
@@ -12,22 +12,23 @@ function getMovies() {
 }
 
 function printMovies(data) {
-    // Capturar el elemento HTML div con el id miLista
+
+    data.forEach(movie => {
+      // Capturar el elemento HTML div con el id miLista
     let moviesListUlElement = document.getElementById("main");
 
     // Crear un elemnto HTML ul en JavaScript
     let ulElement = document.createElement("div");
+    ulElement.setAttribute('class', 'card');
 
     // Añadimos el elemento creado ul al elemento div (miLista)
     moviesListUlElement.appendChild(ulElement);
-
-    data.forEach(movie => {
         // crear el HTML necesario para imprimir una peli
-        let listaHtml = '<h1 class="ejemplo" style="color: red;">'+movie.title+'</h1>';
+        let listaHtml = '<h1 class="card-title" style="color: red;">'+movie.title+'</h1>';
         let directoHtml = '<h2 class="ejemplo" style="color: blue;">'+movie.director+'</h2>';
         let scoreHtml = '<h2 class="ejemplo" style="color: black;">'+movie.score+'</h2>';
-        let synopsisHtml = '<h2 class="ejemplo" style="color: black;">'+movie.synopsis+'</h2>';
-        let coverImage =   '<img src='+movie.coverImage+' class="img-fluid" alt="">'
+        let synopsisHtml = '<h2 class="card-text" style="color: black;">'+movie.synopsis+'</h2>';
+        let coverImage =   '<img src='+movie.coverImage+' class="card-img-top" alt="">'
 
         //li.appendChild(document.createTextNode(movie.title));
         //moviesListUlElement.appendChild(listaHtml);
